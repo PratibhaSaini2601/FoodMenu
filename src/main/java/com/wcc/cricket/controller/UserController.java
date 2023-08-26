e9b18574d58184924bf2b4aa26891f0b73462677
pratibha <pratibha.saini2601@gmail.com>
2023-08-26 16:34:34 +0530

    Moved Post+User controller to UserController.java

--- src/main/java/com/wcc/cricket/controller/UserController.java
+++ src/main/java/com/wcc/cricket/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.wcc.cricket.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wcc.cricket.dao.models.User;
+import com.wcc.cricket.request.RegisterRequest;
+import com.wcc.cricket.response.AuthenticationResponse;
 import com.wcc.cricket.response.UserResponse;
+import com.wcc.cricket.service.AuthenticationService;
 import com.wcc.cricket.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +17,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
     final ObjectMapper mapper = new ObjectMapper();
 
     @GetMapping("users")
-    public ResponseEntity<List<UserResponse>> getUserAllUse() {
+    public ResponseEntity<List<UserResponse>> getUserAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
@@ -25,8 +30,18 @@ public class UserController {
         return ResponseEntity.ok(userService.getUserByID(userID));
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<AuthenticationResponse> createUser(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    }
+
+    @PatchMapping("users/{userID}")
+    public void updateUser(@PathVariable int userID, @RequestBody User user) {
+        userService.updateUser(userID, user);
+    }
+
     @DeleteMapping("users/{userID}")
-    public ResponseEntity<UserResponse> deleteUserByID(@PathVariable int userID) {
-        return ResponseEntity.ok(userService.deleteUserById(userID));
+    public void deleteUser(@PathVariable int userID) {
+        userService.deleteUser(userID);
     }
 }
