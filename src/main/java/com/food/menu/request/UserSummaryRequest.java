ea64f7f1f68cb876d7df60f118ba9e8d5df3bb33
pratibha <pratibha.saini2601@gmail.com>
2023-08-29 23:40:28 +0530

    Added new controller user/summary

---
+++ src/main/java/com/food/menu/request/UserSummaryRequest.java
@@ -0,0 +1,15 @@
+package com.food.menu.request;
+
+import com.food.menu.dao.models.Role;
+import lombok.Getter;
+
+@Getter
+public class UserSummaryRequest {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+    private boolean isDisabled;
+}
